bb546d01fde70817a9c22ae2f1054a9f3eca4ef5
arttu <arttu9994@gmail.com>
2023-02-13 14:14:53 +0200

    feat: add permission constants

---
+++ src/constants/permission/users-permission.ts
@@ -0,0 +1,10 @@
+import { UsersPermission } from 'src/interface';
+
+const UsersPermission: UsersPermission = {
+  CREATE: 'CREATE',
+  READ: 'READ',
+  UPDATE: 'UPDATE',
+  DELETE: 'DELETE',
+};
+
+export default UsersPermission;
