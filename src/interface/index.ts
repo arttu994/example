8e2bc65050a1da12302ebb6ee1577b852085cbe5
arttu <arttu9994@gmail.com>
2023-02-13 14:58:05 +0200

    feat: add roles interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,6 +1,10 @@
 import UsersPermission from './permission.interface';
+import UsersRoles from './roles.interface';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Permission extends UsersPermission {}
 
-export { Permission, UsersPermission };
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Role extends UsersRoles {}
+
+export { Permission, Role, UsersRoles, UsersPermission };
