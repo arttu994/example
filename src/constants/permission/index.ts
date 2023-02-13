f64651451346c4ff253039e5d46ae2bbad9bf440
arttu <arttu9994@gmail.com>
2023-02-13 14:38:11 +0200

    fix: permission constants

---
+++ src/constants/permission/index.ts
@@ -0,0 +1,8 @@
+import { Permission } from '../../interface';
+import UsersPermission from './users-permission';
+
+const permission: Permission = {
+  ...UsersPermission,
+};
+
+export default permission;
