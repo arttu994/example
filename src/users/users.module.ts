4926a1d0cc7dc81123d9b0c03bd9c42dece3fa40
arttu <arttu9994@gmail.com>
2023-02-14 19:05:44 +0200

    fix: users module dependencies

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,6 +6,7 @@ import { Roles } from '../roles/roles.entity';
 import { RolesModule } from '../roles/roles.module';
 import { UsersInitService } from './users-init.service';
 import { Users } from './users.entity';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { Users } from './users.entity';
     PermissionModule,
     RolesModule,
   ],
-  providers: [UsersInitService],
+  providers: [UsersInitService, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
