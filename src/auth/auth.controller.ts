72b684faf57f065677f31edd761a50d33473952e
arttu <arttu9994@gmail.com>
2023-02-16 01:16:39 +0200

    feat: implement signup method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupRequestDto } from './dto/SignupRequest.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,7 +12,7 @@ export class AuthController {
   }
 
   @Post('signup')
-  async signup() {
-    return this.authService.signup();
+  async signup(signupDto: SignupRequestDto) {
+    return this.authService.signup(signupDto);
   }
 }
