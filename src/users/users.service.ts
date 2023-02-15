fa8dadde84ef7a3a0dd5cf1137edfcdb722c8df3
arttu <arttu9994@gmail.com>
2023-02-16 01:27:52 +0200

    feat: add save user method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { SignupRequestDto } from '../auth/dto/SignupRequest.dto';
 import { Users } from './users.entity';
+import * as argon2 from 'argon2';
+import role from '../constants/roles';
+import { RolesService } from '../roles/roles.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private readonly usersRepository: Repository<Users>,
+    private readonly rolesService: RolesService,
   ) {}
 
   async findOneByUsername(username: string) {
     return this.usersRepository.findOne({ where: { username } });
+  }
+
+  async save(user: SignupRequestDto) {
+    const password = await argon2.hash(user.password);
+    const roles = await this.rolesService.findByRole(role.USER);
+
+    const newUser = await this.usersRepository.save({
+      username: user.username,
+      password,
+      roles: [roles],
+    });
+
+    return newUser;
   }
 }
