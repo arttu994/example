934c899fdf95fccb70dc15d1b469e94c3fb62176
arttu <arttu9994@gmail.com>
2023-02-16 22:39:16 +0200

    feat: implement signup method of auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, NotImplementedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { SignupRequestDto } from './dto/SignupRequest.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
   async signin() {
     throw new NotImplementedException();
   }
 
-  async signup() {
-    throw new NotImplementedException();
+  signup(signupDto: SignupRequestDto) {
+    return this.usersService.save(signupDto);
   }
 }
