6512c2a7d3633e856f039408cf0f8aec87bc4196
arttu <arttu9994@gmail.com>
2023-02-16 22:46:06 +0200

    refactor: move SignupRequestDto and SignupResponseDto into separate folder

---
+++ src/auth/dto/signup/SignupResponse.dto.ts
@@ -0,0 +1,22 @@
+import { Exclude } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Roles } from '../../roles/roles.entity';
+
+export class SignupResponseDto {
+  @IsNumber()
+  @IsOptional()
+  id: number;
+
+  @IsString()
+  username: string;
+
+  @Exclude()
+  password: string;
+
+  @Exclude()
+  roles: Roles[];
+
+  constructor(user: Partial<SignupResponseDto>) {
+    Object.assign(this, user);
+  }
+}
---
+++ src/auth/dto/signup/index.ts
@@ -0,0 +1,4 @@
+import { SignupRequestDto } from './SignupRequest.dto';
+import { SignupResponseDto } from './SignupResponse.dto';
+
+export { SignupRequestDto, SignupResponseDto };
