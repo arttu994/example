b487e1c9c70badceb94e35d0e3f0f6bd851f6321
arttu <arttu9994@gmail.com>
2023-03-05 20:46:05 +0200

    feat: add errors and refresh token interfaces, minor fixes

--- src/interface/permission.interface.ts
+++ src/interface/permission.interface.ts
@@ -1,8 +1,20 @@
-interface UsersPermission {
-  CREATE: string;
-  READ: string;
-  UPDATE: string;
-  DELETE: string;
+export interface UsersPermission {
+  USER_CREATE: string;
+  USER_READ: string;
+  USER_UPDATE: string;
+  USER_DELETE: string;
 }
 
-export default UsersPermission;
+export interface ProductPermissions {
+  PRODUCT_CREATE: string;
+  PRODUCT_READ: string;
+  PRODUCT_UPDATE: string;
+  PRODUCT_DELETE: string;
+}
+
+export interface CategoryPermissions {
+  CATEGORY_CREATE: string;
+  CATEGORY_READ: string;
+  CATEGORY_UPDATE: string;
+  CATEGORY_DELETE: string;
+}
